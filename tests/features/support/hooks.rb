10442beb0bfcf1a2e94817ae0f93244e1b4f6f5d
oliveiracleber <oliveira.bnhc@gmail.com>
2019-10-02 03:15:51 -0300

    Removi a pasta

--- tests/features/support/hooks.rb
+++
@@ -1,9 +0,0 @@
-After do |scenario|
-    scenario_name = scenario.name.gsub(/\s+/,'_').tr('/','_')
-
-    if scenario.failed?
-        printscreen(scenario_name.downcase!, 'falhou')
-    else
-        printscreen(scenario_name.downcase!, 'passou')
-    end
-end
--- tests/features/support/page_helper.rb
+++
@@ -1,7 +0,0 @@
-Dir[File.join(File.dirname(__FILE__), "../pages/*_pages.rb")].each {|file| require file}
-
-module Pages
-    def user
-        @user ||= User.new
-    end
-end
